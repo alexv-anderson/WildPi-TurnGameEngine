0cee36bee89d6a4671d9124c153902a2712ed3f7
alexv-anderson <opiemike613@yahoo.com>
2018-02-07 11:35:53 -0600

    Add license

--- src/games/structures/container/turn/GameEngine.java
+++ src/games/structures/container/turn/GameEngine.java
@@ -1,3 +1,7 @@
+/*
+ * Licensed under the Creative Commons Attribution 4.0 International license, 2018, Alex Anderson
+ */
+
 package games.structures.container.turn;
 
 import java.util.Collection;
